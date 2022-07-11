2e883c3573b80580d20cba66c1df28376fcebdd6
parthnanda99 <parth.nanda@zeuslearning.net>
2022-07-11 19:08:41 +0530

    json rendering usng typescript

--- Task 4/dest/dashboard.js
+++ Task 4/dest/dashboard.js
@@ -1,4 +1,14 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// import {product} from './interfaces/interface'
 let box = document.getElementById("box");
 let ham = document.getElementById("hambur");
 let down = false;
@@ -32,3 +42,72 @@ function toggleNotify(e) {
         down1 = false;
     });
 }
+;
+let Alert = document.getElementById("alerts");
+let alertItem = document.getElementById("alert-item");
+let down2 = false;
+function toggleAlert(e) {
+    if (!down2) {
+        Alert.style.height = "582px";
+        Alert.style.opacity = "1";
+        down2 = true;
+        // box.style.display = inline - block;
+    }
+    alertItem.addEventListener("mouseleave", function (e) {
+        Alert.style.height = "0px";
+        Alert.style.opacity = "0";
+        // box.style.display = none;
+        down2 = false;
+    });
+}
+;
+function fetchData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch('../data/data.json');
+        const course = yield response.json();
+        const { data } = course;
+        console.log(data);
+        return data;
+    });
+}
+const renderData = (data) => {
+    const courseContainer = document.querySelector(".courses");
+    data.forEach((item) => {
+        courseContainer.insertAdjacentHTML("beforeend", `  <div class="card">
+            <div class="card-header">
+                <div class="course-image">
+                    <img src=${item.heading.cover} alt="course 1">
+                </div>
+                <div class="course-info">
+                    <div class="title">
+                    ${item.heading.isExpired ? (`<span class="expired">EXPIRED</span>`) : (`<span></span>`)} 
+                        <h2 tabindex="0">${item.heading.title}</h2>
+                        <img class=" ${item.favourite.isDisable ? 'disabled-star' : ''}" src="${item.favourite.icon}" alt="">
+                    </div>
+                    <div class="grade-info"><span class="subject">${item.class.subject}</span><span class="line">|</span><span
+                            class="grade">${item.class.grade}</span>
+                        <span class="green-text">${item.class.greenText}</span></div>
+                    <p class="syllabus"><span class="units bold-text">${item.syllabus.units}</span> ${item.syllabus.unitsText} <span
+                            class="lessons bold-text">${item.syllabus.lessons}</span> Lessons <span class="topics bold-text">${item.syllabus.topics}</span>
+                            ${item.syllabus.topicsText}</p>
+                    <div class="teacher">
+                        <select id="teacher" aria-label="teacher" name="teacher">
+                            <option value="frank">${item.teacher}</option>
+                        </select>
+                    </div>
+                    <p class="students">${item.duration.students}${item.duration.isDashVisible ? (`<span class="line">|</span>`) : (`<span></span>`)} <span class="date">${item.duration.date}  </span></p>
+                </div>
+            </div>
+            <div class="options">
+            <img class="${item.preview.isDisable ? 'disabled' : ''}" src="${item.preview.icon}" alt="preview">
+            <img class="${item.manage.isDisable ? 'disabled' : ''}" src="${item.manage.icon}" alt="manage">
+            <img class="${item.submission.isDisable ? 'disabled' : ''}" src="${item.submission.icon}" alt="submission">
+            <img class="${item.report.isDisable ? 'disabled' : ''}" src="${item.report.icon}" alt="report">
+            </div>
+        </div>`);
+    });
+};
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    const products = yield fetchData();
+    renderData(products);
+}))();
--- Task 4/src/dashboard.ts
+++ Task 4/src/dashboard.ts
@@ -1,3 +1,4 @@
+// import {product} from './interfaces/interface'
 let box = document.getElementById("box") as HTMLElement;
 let ham = document.getElementById("hambur") as HTMLElement; 
 let down = false;
@@ -30,4 +31,134 @@ function toggleNotify(e : any) {
         // box.style.display = none;
         down1 = false;
     });
+};
+
+let Alert = document.getElementById("alerts") as HTMLElement;
+let alertItem = document.getElementById("alert-item") as HTMLElement;
+let down2 = false;
+function toggleAlert(e : any) {
+    if (!down2) {
+        Alert.style.height = "582px";
+        Alert.style.opacity = "1";
+        down2 = true;
+        // box.style.display = inline - block;
+    }
+    alertItem.addEventListener("mouseleave", function (e) {
+        Alert.style.height = "0px";
+        Alert.style.opacity = "0";
+        // box.style.display = none;
+        down2= false;
+    });
+};
+
+
+// interface
+ interface Product {
+    
+    heading: {
+      cover: string;
+      title: string;
+      isExpired : boolean;
+    }
+    favourite: {
+      icon: string;
+      isDisable : boolean;
+    }
+    class:{
+      subject: string;
+      grade:  string;
+      greenText:string;
+    },
+    syllabus:{
+      units: number;
+      unitsText:  string;
+      lessons:number,
+      lessonsText:  string;
+      topics: number
+      topicsText: string;
+    },
+    teacher:string;
+    
+    duration:{
+      students: string;
+      isDashVisible: boolean;
+      date:string;
+    },
+    preview: {
+      icon: string;
+      isDisable : boolean;
+    },
+    manage: {
+      icon:string;
+      isDisable : boolean;
+    
+    },
+    submission: {
+      icon: string;
+      isDisable: boolean;
+    },
+    report: {
+      icon:string;
+      isDisable : boolean;
+    }
+    isExpired : boolean;
+   
+    
+
+ }
+async function fetchData() {
+    const response = await fetch('../data/data.json');
+    const course = await response.json();
+    
+    const { data }  = course;
+    console.log(data);
+    return data;
+   
 }
+
+
+const renderData = ( data : [Product]   ) => {
+    const courseContainer = document.querySelector(".courses") as HTMLElement;
+    data.forEach((item: Product) => {
+        courseContainer.insertAdjacentHTML(
+            "beforeend",
+            `  <div class="card">
+            <div class="card-header">
+                <div class="course-image">
+                    <img src=${item.heading.cover} alt="course 1">
+                </div>
+                <div class="course-info">
+                    <div class="title">
+                    ${item.heading.isExpired ? (`<span class="expired">EXPIRED</span>`) : (`<span></span>`)} 
+                        <h2 tabindex="0">${item.heading.title}</h2>
+                        <img class=" ${item.favourite.isDisable ? 'disabled-star' : ''}" src="${item.favourite.icon}" alt="">
+                    </div>
+                    <div class="grade-info"><span class="subject">${item.class.subject}</span><span class="line">|</span><span
+                            class="grade">${item.class.grade}</span>
+                        <span class="green-text">${item.class.greenText}</span></div>
+                    <p class="syllabus"><span class="units bold-text">${item.syllabus.units}</span> ${item.syllabus.unitsText} <span
+                            class="lessons bold-text">${item.syllabus.lessons}</span> Lessons <span class="topics bold-text">${item.syllabus.topics}</span>
+                            ${item.syllabus.topicsText}</p>
+                    <div class="teacher">
+                        <select id="teacher" aria-label="teacher" name="teacher">
+                            <option value="frank">${item.teacher}</option>
+                        </select>
+                    </div>
+                    <p class="students">${item.duration.students}${item.duration.isDashVisible ? (`<span class="line">|</span>`) : (`<span></span>`)} <span class="date">${item.duration.date}  </span></p>
+                </div>
+            </div>
+            <div class="options">
+            <img class="${item.preview.isDisable ? 'disabled' : ''}" src="${item.preview.icon}" alt="preview">
+            <img class="${item.manage.isDisable ? 'disabled' : ''}" src="${item.manage.icon}" alt="manage">
+            <img class="${item.submission.isDisable ? 'disabled' : ''}" src="${item.submission.icon}" alt="submission">
+            <img class="${item.report.isDisable ? 'disabled' : ''}" src="${item.report.icon}" alt="report">
+            </div>
+        </div>`
+        );
+    })
+};
+   
+(async () =>{
+    const products =  await fetchData();
+    renderData(products);
+    })()
